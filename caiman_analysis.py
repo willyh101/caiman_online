cc8c63a0e68f8e8a9df1a97b0e5463055b3c9c3e
will <hendricksw@gmail.com>
2020-07-18 16:24:39 -0700

    cleaned up analysis main code stuff

--- caiman_analysis.py
+++ caiman_analysis.py
@@ -6,33 +6,29 @@ import h5py
 import caiman as cm
 # from caiman.source_extraction.cnmf import cnmf as cnmf
 import pandas as pd
-from scipy.stats.mstats import zscore
+import scipy.stats as stats
 import json
 import matplotlib.pyplot as plt
+import sklearn
+import numpy as np
 
 def load_json(path):
     with open(path, 'r') as file:
         data_json = json.load(file)
     return data_json
 
-def make_traces_from_json(path):
-    """Short cut for loading a json from path and 
-    making it into traces=(trials x cell x time)."""
-    data = load_json(path)
-    traces = make_trialwise(data['c'], data['splits'])
-    return traces
-
-def load_data(caiman_data_path):
-    with h5py.File(caiman_data_path) as f:
-        traces = f['estimates']['C'][()]
-    return traces
-
-def make_images(caiman_obj):
-    Yr, dims, T = cm.load_memmap(caiman_obj.mmap_file)
-    return np.reshape(Yr, [T] + list(dims), order='F')
-
 def load_as_obj(caiman_data_path):
+    """hdf5 -> caiman object"""
     return cm.source_extraction.cnmf.cnmf.load_CNMF(caiman_data_path)
+
+def make_traces_from_json(path, *args, **kwargs):
+    """
+    Short cut for loading a json from path and making it into 
+    traces=(trials x cell x time). Passes any args and kwargs to process_data.
+    """
+    data = load_json(path)
+    traces = process_data(data['c'], data['splits'], *args, **kwargs)
+    return traces
 
 def make_trialwise(traces, splits):
     """Returns trial x cell x time."""
@@ -40,21 +36,55 @@ def make_trialwise(traces, splits):
     shortest = min([s.shape[1] for s in traces])
     return np.array([a[:, :shortest] for a in traces])
 
+def make_images(caiman_obj):
+    Yr, dims, T = cm.load_memmap(caiman_obj.mmap_file)
+    return np.reshape(Yr, [T] + list(dims), order='F')
+
 def find_com(A, dims, x_1stPix):
     XYcoords= cm.base.rois.com(A, *dims)
     XYcoords[:,1] = XYcoords[:,1] + x_1stPix #add the dX from the cut FOV
     i = [1, 0]
     return XYcoords[:,i] #swap them
 
-def process_data(c, splits):
-    # do zscore and subtract off min
-    # zscore_data = zscore(c)
-    # zscore_data -= zscore_data.min(0, keepdims=True)
+def process_data(c, splits, normalizer='minmax', func=None, *args, **kwargs):
+    """
+    Processes temporal data (taken from C) by subtracting off min for each cell and then 
+    optionally normalizing it on axis=1 (aka cells). Can use minmax (default), zscore, 
+    normalize, or scale. See sklearn.preprocessing for more info. Alternatively can pass
+    'other' to use a custom function (func) and passes *args and **kwargs to that function.
+    Finally, uses splits to make the data trialwise into trial x cells x time numpy array.
+
+    Args:
+        c ([type]): [description]
+        splits ([type]): [description]
+        normalizer (str, optional): [description]. Defaults to 'minmax'.
+
+    Returns:
+        [type]: [description]
+    """
+    # make sure it's an array
+    if not isinstance(c, np.ndarray):
+        c = np.array(c)
     
-    # make trialwise -> trials x cell x time
-    # traces = make_trialwise(zscore_data, splits)
-    c = np.array(c)
-    traces = make_trialwise(c, splits)
+    # subtract off min cellwise to zero the traces
+    data = c - c.min(axis=1).reshape(-1, 1)
+    
+    # normalization routines
+    if normalizer == 'other':
+        normed_data = func(*args, **kwargs)
+    else:
+        norm_routines = {
+            'none': data, # nothing done...
+            'minmax': sklearn.preprocessing.minmax_scale(data, axis=1), # scaled to min max (not abs)
+            'zscore': stats.zscore(data, axis=1), # old fashion zscoring
+            'norm': sklearn.preprocessing.normalize(data, axis=1), # L2 norm
+            'scale': sklearn.preprocessing.scale(data, axis=1),
+        }
+        
+        normed_data = norm_routines[normalizer]
+    
+    traces = make_trialwise(normed_data, splits)
+    
     return traces
 
 def concat_chunked_data(jsons):
@@ -93,7 +123,6 @@ def posthoc_dff_and_coords(cm_obj):
     coords = cm.utils.visualization.get_contours(cm_obj.estimates.A, dims=cm_obj.dims)
     
     return dff, coords
-
 
 def extract_cell_locs(cm_obj):
     """
@@ -136,48 +165,3 @@ def cell_locs_multifile(cm_objs):
     out_df['sum_var'] = out_df['y_var'] + out_df['x_var']
     
     return out_df.reset_index()
-    
-# def extract_cell_locs(cm_obj):
-#     """
-#     Get the center-of-mass and coordinates(countors) of all cells from a caiman object. Loads
-#     directly from caiman obj or from a string/path and loads the caiman obj.
-
-#     Args:
-#         cm_obj ([caiman, str]): caiman object or path to caiman object
-
-#     Returns:
-#         center of mass, coordinates (aka countours/cell edges)
-#     """
-#     if isinstance(cm_obj, str):
-#         cm_obj = load_as_obj(cm_obj)
-#     cell_coords = cm.utils.visualization.get_contours(cm_obj.estimates.A, dims=cm_obj.dims)
-    
-#     coms = np.array([c['CoM'] for c in cell_coords])
-#     coords = np.array([c['coordinates'] for c in cell_coords])
-#     ids = np.array([c['neuron_id']])
-    
-#     return coms, coords
-
-# def mean_and_var_locs(cm_objs):
-#     """
-#     Get mean and variance of cell locations across multiple hdf5 outputs for all cells in 
-#     each FOV. Calculates across files. Returns sum variance per target (x.var() + y.var())
-
-#     Args:
-#         cm_objs (list): list of caiman objects or path to caiman objs to mean over
-
-#     Returns:
-#         mean: np.array (cells x xy)
-#         variance: np.array(cells x var)
-#     """
-    
-#     coms = []
-#     for c in cm_objs:
-#         com_temp, _ = extract_cell_locs(c)
-#         coms.append(com_temp)
-
-#     coms = np.array(coms)
-#     means = coms.mean(axis=0)
-#     varis = coms.var(axis=0).sum(axis=1)
-
-#     return means, varis
